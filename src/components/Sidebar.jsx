ff0e55b60a16aec53219b41ae4b035532a69c8a0
wangzi <wangzi2124@outbook.com>
2026-05-08 19:53:35 +0800

    bug修复

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -21,7 +21,7 @@ function Sidebar() {
     setActiveModelData(getActiveModel())
   }, [activeModelId, getActiveModel])
 
-  const allModels = [...models]
+  const allModels = [...models, ...customModels]
 
   const handleAddModel = () => {
     addModel({
